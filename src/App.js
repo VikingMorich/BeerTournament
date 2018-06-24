10d739b795c3ab91574321e8316c509f07dc8ddd
Enric Moriche <Morich@MacBook-Pro-de-Enric.local>
2018-06-24 21:18:06 +0200

    tittle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// using ES6 modules
+import { Router, Route, Switch } from 'react-router';
 
 class App extends Component {
   render() {
@@ -8,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">🤘🏼🍺 - Beer Tournament - 🍺🤘🏼</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
